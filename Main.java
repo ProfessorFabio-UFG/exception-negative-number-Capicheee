768ee6a15a90a6140bf7576d921a74437075b3f9
JulianoKenzo <141460497+Capicheee@users.noreply.github.com>
2025-05-30 15:49:13 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
-public class Main{
-    private static double raizQuadrada(double x){
-        if(x<0){
-            throw new NumeroNegativo("Raíz de número negativo.");
+public class Main {
+    private static double raizQuadrada(double x) {
+        if (x < 0){
+            throw new NumeroNegativo("Raiz de numero negativo nao permitida.");
         }
         return Math.sqrt(x);
     }
 
     public static void main(String[] args){
-        System.out.println("Digite um número não negativo: ");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero nao negativo: ");
         double x = sc.nextDouble();
-        double r = 0;
+        
         try{
-
+            double r = raizQuadrada(x);
+            System.out.println("Raiz quadrada: " + r);
+        } 
+        catch (NumeroNegativo e){
+            System.out.println("Erro: " + e.getMessage());
+        }
+        finally{
+            sc.close();
         }
     }
 }
